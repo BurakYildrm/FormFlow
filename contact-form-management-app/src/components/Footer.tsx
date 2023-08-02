1cb88e352c934148cac8b08791732d80a9a568fb
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-03 02:06:23 +0300

    Footer is added.

---
+++ contact-form-management-app/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+	return (
+		<footer className="footer footer-center p-4 bg-base-300 text-base-content">
+			<div>
+				<p>
+					Copyright &copy; 2023 - All right reserved by FormFlow Ltd
+				</p>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
