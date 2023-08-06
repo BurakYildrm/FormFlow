8b2e7a32c72f06f49e99bd13eb0e0623fa744af1
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-06 17:19:30 +0300

    LocalizedCombobox, Select and Alert is added.

--- contact-form-management-app/src/components/index.ts
+++ contact-form-management-app/src/components/index.ts
@@ -2,5 +2,16 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import ContactForm from "./ContactForm";
 import LoginForm from "./LoginForm";
+import LocalizedCombobox from "./LocalizedCombobox";
+import Select from "./Select";
+import Alert from "./Alert";
 
-export { Footer, Navbar, ContactForm, LoginForm };
+export {
+	Footer,
+	Navbar,
+	ContactForm,
+	LoginForm,
+	LocalizedCombobox,
+	Select,
+	Alert,
+};
