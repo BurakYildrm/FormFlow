2471d46f4763464334dfc9ab9c7c70150bd5a5d6
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-06 16:36:34 +0300

    Loading pages are added.

---
+++ contact-form-management-app/src/app/[locale]/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+	return (
+		<div className="flex-1 flex flex-col items-center justify-center">
+			<span className="loading loading-infinity loading-lg"></span>
+		</div>
+	);
+}
