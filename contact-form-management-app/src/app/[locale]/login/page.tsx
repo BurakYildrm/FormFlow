7d8df46d8f2327eaa7012bf35066dbba3a778c97
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-06 17:17:25 +0300

    Localization for login page is added.

--- contact-form-management-app/src/app/[locale]/login/page.tsx
+++ contact-form-management-app/src/app/[locale]/login/page.tsx
@@ -1,18 +1,39 @@
-import { Metadata } from "next";
 import React from "react";
 import { LoginForm } from "@/components";
+import pick from "lodash/pick";
+import {
+	useMessages,
+	useLocale,
+	NextIntlClientProvider,
+	AbstractIntlMessages,
+} from "next-intl";
+import { getTranslator } from "next-intl/server";
 
-export const metadata: Metadata = {
-	title: "Login",
-	description: "Login",
-};
+export async function generateMetadata() {
+	const locale = useLocale();
+	const t = await getTranslator(locale, "Metadata");
+
+	return {
+		title: t("login"),
+	};
+}
 
 const Login = () => {
+	const messages = useMessages();
+	const locale = useLocale();
+
 	return (
 		<>
-			<div className="flex flex-col items-center justify-center min-h-screen bg-base-100">
-				<div className="container max-w-screen-sm mx-auto px-2">
-					<LoginForm />
+			<div className="flex-1 flex flex-col items-center justify-center bg-base-100">
+				<div className="container max-w-screen-sm mx-auto px-2 flex justify-center items-center">
+					<NextIntlClientProvider
+						locale={locale}
+						messages={
+							pick(messages, "LoginForm") as AbstractIntlMessages
+						}
+					>
+						<LoginForm />
+					</NextIntlClientProvider>
 				</div>
 			</div>
 		</>
