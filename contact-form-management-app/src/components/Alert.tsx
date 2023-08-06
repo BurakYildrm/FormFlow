a4c5afe0381bb63983ba786099e5c657aab62805
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-06 17:18:29 +0300

    Background color bug is fixed.

--- contact-form-management-app/src/components/Alert.tsx
+++ contact-form-management-app/src/components/Alert.tsx
@@ -6,7 +6,7 @@ interface AlertProps {
 }
 
 const Alert: React.FC<AlertProps> = (props: AlertProps) => {
-	const svgPath = {
+	const svgPaths = {
 		info: "M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z",
 		success: "M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z",
 		warning:
@@ -14,8 +14,17 @@ const Alert: React.FC<AlertProps> = (props: AlertProps) => {
 		error: "M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z",
 	};
 
+	const colors = {
+		info: "alert-info",
+		success: "alert-success",
+		warning: "alert-warning",
+		error: "alert-error",
+	};
+
 	return (
-		<div className={`alert alert-${props.type}`}>
+		<div
+			className={`alert ${colors[props.type as keyof typeof colors]} p-2`}
+		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				className="stroke-current shrink-0 h-6 w-6"
@@ -26,10 +35,10 @@ const Alert: React.FC<AlertProps> = (props: AlertProps) => {
 					strokeLinecap="round"
 					strokeLinejoin="round"
 					strokeWidth="2"
-					d={svgPath[props.type as keyof typeof svgPath]}
+					d={svgPaths[props.type as keyof typeof svgPaths]}
 				/>
 			</svg>
-			<span>{props.message}</span>
+			<span className="pr-4">{props.message}</span>
 		</div>
 	);
 };
