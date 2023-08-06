c129a702cc2b7a6acb7d52957d1ec6681874e4af
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-06 23:02:15 +0300

    Not found page metadata localization is added.

--- app/src/app/[locale]/[...not-found]/page.tsx
+++ app/src/app/[locale]/[...not-found]/page.tsx
@@ -1,4 +1,15 @@
+import { useLocale } from "next-intl";
+import { getTranslator } from "next-intl/server";
 import { notFound } from "next/navigation";
+
+export async function generateMetadata() {
+	const locale = useLocale();
+	const t = await getTranslator(locale, "Metadata");
+
+	return {
+		title: t("notFound"),
+	};
+}
 
 export default function NotFoundDummy() {
 	notFound();
