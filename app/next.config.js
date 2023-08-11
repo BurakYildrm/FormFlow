fd174c6ef36a57b5d413c5e341609ac51dc11940
Burak Yıldırım <yldrmburak1167@gmail.com>
2023-08-11 04:17:46 +0300

    svgn support is added.

--- app/next.config.js
+++ app/next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
+
 	experimental: {
 		serverActions: true,
 	},
 };
 
-const withNextIntl = require("next-intl/plugin")(
-	// This is the default (also the `src` folder is supported out of the box)
-	"./src/i18n.ts"
-);
+const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
 module.exports = withNextIntl({
 	...nextConfig,
